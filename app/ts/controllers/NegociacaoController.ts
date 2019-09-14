964710c9db5542841a9f5b4e33fe87c97f9b1b5a
AlexSerodio <alequis01@gmail.com>
2019-09-14 12:10:37 -0300

    adicionado geração dinâmica de tabela

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,14 +3,17 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update();
     }
 
-    adiciona(event: Event) {
+    adiciona(event: Event): void {
 
         event.preventDefault();
 
@@ -20,7 +23,13 @@ class NegociacaoController {
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+
+        this._negociacoes.paraArray().forEach(negociacao => {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor);
+        })
     }
 
 }
